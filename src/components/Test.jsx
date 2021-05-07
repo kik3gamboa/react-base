01baab2aa88a6f7e070bb4ca03b9ef39c6ebe3e4
Kik3Gamboa <lgamboa@railtronix.com>
2021-05-06 19:35:21 -0500

    first Commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Test = () => {
+    const Hello = "Doctor Tocino"
+    return(
+        <h1>{Hello}</h1>
+    )
+}
+
+export default Test;
